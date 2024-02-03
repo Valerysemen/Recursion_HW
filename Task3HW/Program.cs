93f10353b2e09023524a83fc027427a75e38bf37
Valery <6488671s@gmail.com>
2024-02-03 18:16:20 +0300

    add Task3HW

---
+++ Task3HW/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 3: Задайте произвольный массив. 
+//Выведете его элементы, начиная с конца. 
+//Использовать рекурсию, не использовать циклы
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = { 7, 9, 3, 1, 5 };
+        PrintArrayReversed(array, array.Length - 1);
+    }
+
+    static void PrintArrayReversed(int[] arr, int index)
+    {
+        if (index < 0)  
+        {
+            return;
+        }
+        else
+        {
+            Console.WriteLine(arr[index]); 
+            PrintArrayReversed(arr, index - 1);
+        }
+    }
+}
+
